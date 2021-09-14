64e7a16fb0bf06b7ec2630bb545c2952a5aef03f
naftalimurgor <murgornaftali@gmail.com>
2021-09-15 00:21:24 +0300

    Add truffle-config for deploying smart contracts

---
+++ dapp/truffle-config.js
@@ -0,0 +1,32 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = '<ADD-YOUR-MNEMONIC-HERE>';
+const privateKeyTest = '<ADD-YOUR-PRIVATE-KEY-HERE>';
+
+module.exports = {
+  networks: {
+    testnet: {
+      provider: () => {
+        return new HDWalletProvider({
+          mnemonic,
+          providerOrUrl: 'https://api.s0.b.hmny.io', // https://api.s0.t.hmny.io for mainnet
+          derivationPath: `m/44'/1023'/0'/0/`
+        });
+      },
+      network_id: 1666700000, // 1666600000 for mainnet
+    },
+    testnetHar: {
+      provider: () => {
+        if (!privateKeyTest.trim()) {
+          throw new Error(
+            'Please enter a private key with funds, you can use the default one'
+          );
+        }
+        return new HDWalletProvider({
+          privateKeys: [privateKeyTest],
+          providerOrUrl: 'https://api.s0.b.hmny.io',
+        });
+      },
+      network_id: 1666700000,
+    },
+  },
+};
