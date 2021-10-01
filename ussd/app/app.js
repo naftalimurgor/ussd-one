0e5f36688b9527086f040e985db3077da619ef42
naftalimurgor <murgornaftali@gmail.com>
2021-10-01 17:30:35 +0300

    add morgan logger

--- ussd/app/app.js
+++ ussd/app/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const Web3 = require('web3')
 const dotenv = require('dotenv')
 const USSDAPP = require('./routes')
@@ -15,19 +16,21 @@ const main = () => {
   const PKEY  = process.env.PKEY
 
   const CONTRACT_ADDRESS = '0x1994B83e049E051870DB906e9bEF9CAdEFf8d698'
-  const contractABI = require('./abi').abi
+  const contractABI = require('./abi/abi.json')
+  console.log(contractABI);
   const web3 = new Web3(provider)
-  const contractInstance = new web3.eth.Contract(contractABI, CONTRACT_ADDRESS)
+  const contractInstance = new web3.eth.Contract(contractABI.abi, CONTRACT_ADDRESS)
 
-  const client = new Client(provider, contractABI, contractInstance, web3)
+  const client = new Client(contractInstance, web3)
   const ussd = new USSDAPP(router, client)
 
   // enable json parsing
   app.use(express.json())
+  app.use(morgan('tiny'))
   // routes
-  app.use('/create-wallet', ussd.createWallet())
-  app.use('/get-balance', ussd.getBalance())
-  app.use('/send-one', ussd.sendOne())
+  app.use('/api', ussd.createWallet())
+  app.use('/api', ussd.getBalance())
+  app.use('/api', ussd.sendOne())
 
   app.listen(PORT, () => console.log(`listening on PORT: 0.0.0.0:${PORT}`))
 }
